4e679e0b36ee3bd8d9b3283cba23166dee66cd50
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-01-08 23:45:32 -0500

    se suben los scripts antes de lo pedido

---
+++ selecciones/creacion_estudiantes.sql
@@ -0,0 +1,41 @@
+create table estudiantes(
+	cedula char(10) not null ,
+	nombre varchar(50)not null ,
+	apellido varchar(50)not null ,
+	email varchar(50)not null,
+	fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key(cedula)
+)
+--1
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1102547890','Jhon','Guaman','juan.perez01@example.com'
+,'2025/01/15')
+--2
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1103654781','Juan','Perez','maria.garcia22@testmail.com'
+,'1987/02/25')
+--3
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1104789652','Andy','Guaman' , 'carlos.lopez33@correo.com'
+,'2000/09/25')
+--4
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1105896324','Ivan','Guaman','ana.mendoza44@fakemail.org',
+'2001/10/23')
+--5
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1106987453','Martha','Sanmartin','jose.ramirez55@demo.net',
+'1958/03/01')
+--6
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1107458963','Rita','Morocho','luisa.torres66@prueba.com',
+'1923/07/23')
+--7
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1108963254','Maria','Gualan','andres.sanchez77@sample.org','2003/06/09')
+--8
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('1109658745','Josue','Samaniego','paola.castillo88@mockmail.net'
+,'2002/09/25')
+
+select * from estudiantes
---
+++ selecciones/creacion_productos.sql
@@ -0,0 +1,28 @@
+create table productos(
+	codigo int not null ,
+	nombre varchar(50) not null ,
+	descripcion varchar(200) ,
+	precio money not null ,
+	stock int not null ,
+	constraint productos_pk primary key(codigo) 
+
+)
+--1
+insert into productos(codigo, nombre , descripcion , precio , stock)
+values ('48291736', 'Golosinas', 'colombinas', 0.15,45)
+--2
+insert into productos(codigo, nombre , descripcion , precio , stock)
+values ('93015482', 'limpieza', 'Deja', 1 ,12)
+--3
+insert into productos(codigo, nombre , descripcion , precio , stock)
+values ('71520984', 'Gadyets', 'decorador de oficina', 250.99,2)
+--4
+insert into productos(codigo, nombre , descripcion , precio , stock)
+values ('26849301', 'Dormitorio', 'almohadas', 5.00 ,15)
+--5
+insert into productos(codigo, nombre , descripcion , precio , stock)
+values ('59732048', 'Control de plagas ', 'raid ', 2.41, 12)
+
+
+select * from productos
+
