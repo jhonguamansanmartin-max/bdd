649be29798ce429aae9dac9bd94075af0a617355
jhonguamansanmartin-max <jhonguamansamartin@gmail.com>
2026-01-09 01:35:10 -0500

    personas aumentado

---
+++ inserciones/scrips/personas_aumentado el where.sql
@@ -0,0 +1,6 @@
+select * from personas 
+--where es condicion para buscar con el nombre de la columna 
+where estatura>1.70
+
+insert into personas(cedula,nombre,apellido,fecha_nacimiento)
+values ('1150119787','aaron','samaniego','10/06/1995')
